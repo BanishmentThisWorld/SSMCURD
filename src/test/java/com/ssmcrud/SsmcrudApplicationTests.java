e1950f9142298953bacfc893955dc61210e9b27f
11585 <1158556232@qq.com>
2020-06-15 12:13:09 +0800

    一个ssm项目的CURD

---
+++ src/test/java/com/ssmcrud/SsmcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssmcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmcrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
